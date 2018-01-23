73313bce2dd43e9c6284e15b12fb86170bc1633c
609974309@qq.com <609974309@qq.com>
2018-01-23 15:36:48 +0800

    add SEWeChatTool,etc

---
+++ SEWeChatToolDemo/SEWeChatToolDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SEWeChatToolDemo
+//
+//  Created by sense on 2018/1/22.
+//  Copyright © 2018年 sense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
